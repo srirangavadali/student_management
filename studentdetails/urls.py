88222b5270fb9106a37c0f38c38c88045cfdebc9
prahlad <prahlad1989@gmail.com>
2022-02-24 02:24:27 +0530

    made some changes

--- studentdetails/urls.py
+++ studentdetails/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name = 'index'),
-    path('all',views.all,name='all'),
     path('rec_data',views.rec_data,name='rec_data'),
+    path('all',views.all,name='all'),
 
 
 ]
